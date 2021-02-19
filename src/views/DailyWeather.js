d1198ef23c280bf0416fc58b5e84bfdd6535c234
Blake Ferm <blakeferm@gmail.com>
2021-02-19 12:02:02 -0500

    update repo

--- src/views/DailyWeather.js
+++ src/views/DailyWeather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormatTime from "../utils/FormatTime";
+import Accordion from "react-responsive-accordion";
 
 const DailyWeather = ({ timezone, dailyWeather }) => {
   // round the decimal from dailyWeather.temp
@@ -17,27 +18,31 @@ const DailyWeather = ({ timezone, dailyWeather }) => {
     return FormatTime(dateToFormat, timezone, "MM/DD/YYYY");
   };
   return (
-    <div className="weather-data">
-      <p className="daily-weather-tagline">
-        8 Day Forecast |{" "}
-        <span className="daily-weather-data-city">{timezone}</span>
-      </p>
-      <div className="daily-weather-data-box">
-        {dailyWeather.map((day, idx) => {
-          return (
-            <span className="daily-weather-data-property" key={idx}>
-              <p className="weather-data-title">
-                Date | Time | Temperature | Conditions
-              </p>
-              <p className="daily-weather-data-value">
-                {formatDate(day.dt)} - {formatTime(day.dt)} -{" "}
-                {roundTemp(day.temp.day)}&#176; - {day.weather[0].main}
-              </p>
-            </span>
-          );
-        })}
+    <Accordion>
+      <div className="weather-data">
+        <p className="daily-weather-tagline">
+          8 Day Forecast |{" "}
+          <span className="daily-weather-data-city">{timezone}</span>
+        </p>
+        <div data-trigger="8 Day Forecast >>">
+          <div className="daily-weather-data-box">
+            {dailyWeather.map((day, idx) => {
+              return (
+                <span className="daily-weather-data-property" key={idx}>
+                  <p className="weather-data-title">
+                    Date | Time | Temperature | Conditions
+                  </p>
+                  <p className="daily-weather-data-value">
+                    {formatDate(day.dt)} - {formatTime(day.dt)} -{" "}
+                    {roundTemp(day.temp.day)}&#176; - {day.weather[0].main}
+                  </p>
+                </span>
+              );
+            })}
+          </div>
+        </div>
       </div>
-    </div>
+    </Accordion>
   );
 };
 
