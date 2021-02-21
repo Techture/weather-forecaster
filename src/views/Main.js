37dfcb9f09503a256d3262c6232df6115b02ddfe
Blake Ferm <blakeferm@gmail.com>
2021-02-20 19:15:58 -0500

    update search feature - added list of cities and functionality to filter those cities - almost there

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Header from "../views/layout/Header";
+import WeatherSearch from "../components/WeatherSearch";
 import CurrentWeatherData from "../views/CurrentWeatherData";
 import HourlyWeatherData from "../views/HourlyWeatherData";
 import DailyWeatherData from "../views/DailyWeatherData";
 import Error from "../components/Error";
 import Footer from "../views/layout/Footer";
 
+// list of cities
+import cities from "cities.json";
+
 // api key is provided for the review, and will be deleted when review is complete
 const API_KEY = "f4d7207b99b6170b521b3903384b9293";
 
 const Main = () => {
+  const [userLocation, setUserLocation] = useState({
+    latitude: 41,
+    longitude: -73,
+    name: "",
+  });
   const [currentWeather, setCurrentWeather] = useState(null);
   const [hourlyWeather, setHourlyWeather] = useState(null);
   const [dailyWeather, setDailyWeather] = useState(null);
@@ -20,37 +29,43 @@ const Main = () => {
   const [error, setError] = useState(null);
 
   // get user location
-  const fetchUserLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        let pos = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-        };
+  const fetchUserLocation = async () => {
+    try {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          let fetchedPostion = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
 
-        // use onecall endpoint for current, hourly and daily weather info
-        const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${pos.latitude}&lon=${pos.longitude}&appid=${API_KEY}&units=imperial`;
+          setUserLocation({ userLocation: fetchedPostion });
 
-        axios.get(url).then((res) => {
-          let d = res.data;
+          // use onecall endpoint for current, hourly and daily weather info
+          const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${fetchedPostion.latitude}&lon=${fetchedPostion.longitude}&appid=${API_KEY}&units=imperial`;
 
-          // format the value of timezone to get just the city
-          let cityFormatter = d.timezone
-            .split("/")
-            .slice(1, 2)
-            .join(" ")
-            .split("_")
-            .join(" ");
+          axios.get(url).then((res) => {
+            let d = res.data;
 
-          setCurrentWeather(d.current);
-          setHourlyWeather(d.hourly);
-          setDailyWeather(d.daily);
-          setTimezone(d.timezone);
-          setCity(cityFormatter);
-          setConditions(d.current.weather[0].main);
-          setError(null);
+            // format the value of timezone to get just the city
+            let cityFormatter = d.timezone
+              .split("/")
+              .slice(1, 2)
+              .join(" ")
+              .split("_")
+              .join(" ");
+
+            setCurrentWeather(d.current);
+            setHourlyWeather(d.hourly);
+            setDailyWeather(d.daily);
+            setTimezone(d.timezone);
+            setCity(cityFormatter);
+            setConditions(d.current.weather[0].main);
+            setError(null);
+          });
         });
-      });
+      }
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -59,7 +74,9 @@ const Main = () => {
     try {
       e.preventDefault();
 
-      const location = e.target.elements.city.value;
+      let location = e.target.elements.city.value;
+      // console.log("location >>", location);
+      // console.log("cities >>", cities);
 
       if (!location && !city) {
         return (
@@ -67,8 +84,38 @@ const Main = () => {
         );
       }
 
+      // for (const cityScan of cities) {
+      //   return console.log(
+      //     `city: ${cityScan.name} lat: ${cityScan.lat} long: ${cityScan.lng}`
+      //   );
+
+      // TODO
+      // create a function that takes the name of the city a user types in, and filters out the matching city name, updating the lat/long props of that city, which will be passed as two existing properties into the url, pulling that city's data into the UI.
+
+      // const getCity = (cityToGet) => {
+      //   const filteredCity = cities.filter((c) => {
+      //     if (cityToGet === c) {
+      //       return cityToGet.name;
+      //     }
+      //     return console.log(filteredCity);
+      //   });
+      // };
+
+      // getCity(location);
+      // console.log("location >>", location);
+
+      let newLocation = {
+        name: cities[0].name,
+        latitude: cities[0].lat,
+        longitude: cities[0].lng,
+      };
+      setUserLocation({ userLocation: newLocation });
+
       // use onecall endpoint for current, hourly and daily weather info
-      const url = `https://api.openweathermap.org/data/2.5/onecall?${location}&appid=${API_KEY}&units=imperial`;
+      const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${newLocation.latitude}&lon=${newLocation.longitude}&appid=${API_KEY}&units=imperial`;
+
+      // console.log("userLocation >>", userLocation);
+      // console.log("URL >>", url);
 
       const request = axios.get(url);
       const { data } = await request;
@@ -78,17 +125,16 @@ const Main = () => {
       setDailyWeather(data.daily);
       setTimezone(data.timezone);
       setConditions(data.current.weather[0].main);
-      setCity(data.name);
+      setCity(newLocation.name);
       setError(null);
-
-      console.log("DATA 2 >>", data);
     } catch (err) {
-      console.log(err);
+      console.log("fetchWeatherData error >>", err);
     }
   };
 
   useEffect(() => {
     fetchUserLocation();
+
     const localCityName = localStorage.getItem("city");
     if (localCityName) {
       setCity(JSON.parse(localCityName));
@@ -103,6 +149,7 @@ const Main = () => {
     <>
       <div className="main">
         <Header />
+        <WeatherSearch fetchWeatherData={fetchWeatherData} city={city} />
         {error !== null && <p>{<Error error={error} />}</p>}
         {currentWeather !== null && (
           <CurrentWeatherData
