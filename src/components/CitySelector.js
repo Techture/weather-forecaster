e724d92c69150b039a6d13b7b210009b70f08982
Blake Ferm <blakeferm@gmail.com>
2021-02-22 14:03:16 -0500

    update repo

--- src/components/CitySelector.js
+++ src/components/CitySelector.js
@@ -6,6 +6,27 @@ import { FormControl, Button } from "react-bootstrap";
 const CitySelector = ({ onSearch }) => {
   const [city, setCity] = useState("");
 
+  // as the user types a city, the city is matched with its lat/lng
+  // helper to match the city names
+  //     const cleanCityName = (cityName) => {
+  //       return cityName.toLowerCase();
+  //     };
+
+  //     const updateUserLocation = () => {
+  //       for (const cityIndex of cities) {
+  //         if (cleanCityName(cityIndex.name) === cleanCityName(city)) {
+  //           setCityLocation({
+  //             latitude: cityIndex.lat,
+  //             longitude: cityIndex.lng,
+  //             name: cityIndex.name,
+  //           });
+  //           setCity(cityIndex.name);
+  //         }
+  //       }
+  //     };
+
+  //     updateUserLocation();
+
   const [userLocation, setUserLocation] = useState({
     lat: 64.128288,
     lng: -21.827774,
@@ -45,7 +66,10 @@ const CitySelector = ({ onSearch }) => {
       <div className="weather-search-submit">
         <Button
           className="weather-search-button"
-          onClick={() => onSearch(city, userLocation.lat, userLocation.lng)}
+          onClick={(e) => {
+            e.preventDefault();
+            onSearch(city, userLocation.lat, userLocation.lng);
+          }}
         >
           <FaArrowRight />{" "}
         </Button>
