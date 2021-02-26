2655e6975d2b6761f3fe7237ad978bec8600cb02
Blake Ferm <blakeferm@gmail.com>
2021-02-26 03:09:44 -0500

    update repo

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,2 +1,4 @@
-export const API_KEY = ["f4d7207b99b6170b521b3903384b9293"];
+import { apiKey } from "./secret";
+
+export const API_KEY = [apiKey];
 export const API_BASE_URL = "https://api.openweathermap.org/";
