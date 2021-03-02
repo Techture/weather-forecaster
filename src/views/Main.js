daf2ffe55ce5d7eec89c29996a7ede60ee22a77f
Blake Ferm <blakeferm@gmail.com>
2021-03-02 15:51:32 -0500

    update repo

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Header from "../views/layout/Header";
 import fetchWeatherData from "../utils/FetchWeatherData";
-// import getCurrentLocation from "../utils/GetCurrentLocation";
+import getCurrentLocation from "../utils/GetCurrentLocation";
 import CitySelector from "../components/CitySelector";
 import Weather from "../components/Weather";
 import Footer from "../views/layout/Footer";
@@ -13,16 +13,14 @@ const Main = () => {
     name: "",
   });
   const [weatherData, setWeatherData] = useState(null);
-  // const [loaded, setLoaded] = useState({ loaded: false });
 
   useEffect(() => {
-    //   if (loaded === false) {
-    //     // getCurrentLocation(setUserLocation, setWeatherData);
-    //     setLoaded({ loaded: true });
-    //   }
-
-    fetchWeatherData(userLocation.lat, userLocation.lon, setWeatherData);
-  }, [userLocation]);
+    if (!weatherData) {
+      getCurrentLocation(setUserLocation, setWeatherData);
+    } else if (weatherData) {
+      fetchWeatherData(userLocation.lat, userLocation.lon, setWeatherData);
+    }
+  }, [userLocation.name]);
 
   return (
     <>
