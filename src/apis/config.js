af368aa4779dc80731aed4b5c7e6132346225783
Blake Ferm <blakeferm@gmail.com>
2021-03-04 13:52:06 -0500

    update readme

--- src/apis/config.js
+++ src/apis/config.js
@@ -1,4 +1,4 @@
 import { apiKey } from "./secret";
 
-export const API_KEY = [apiKey];
+export const API_KEY = [apiKey]; //your api key goes here
 export const API_BASE_URL = "https://api.openweathermap.org/";
