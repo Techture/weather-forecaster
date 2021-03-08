d8d48a2ff51fe14a20121689d52867c1d35918f2
Blake Ferm <blakeferm@gmail.com>
2021-03-08 15:25:43 -0500

    update repo

---
+++ build/precache-manifest.a655dc6a50934ee21dfd404d13fa89df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41463de85a34c87d40abad272e82b487",
+    "url": "/Techture/weather-forecaster/tree/bf-demo-link/index.html"
+  },
+  {
+    "revision": "9383d8dc51eca0740232",
+    "url": "/Techture/weather-forecaster/tree/bf-demo-link/static/css/main.2c05eb4e.chunk.css"
+  },
+  {
+    "revision": "3671ea62a5a160b93baf",
+    "url": "/Techture/weather-forecaster/tree/bf-demo-link/static/js/2.b1a7acb3.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/Techture/weather-forecaster/tree/bf-demo-link/static/js/2.b1a7acb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9383d8dc51eca0740232",
+    "url": "/Techture/weather-forecaster/tree/bf-demo-link/static/js/main.58713035.chunk.js"
+  },
+  {
+    "revision": "ab120eafa1945c840c0e",
+    "url": "/Techture/weather-forecaster/tree/bf-demo-link/static/js/runtime-main.35337d0d.js"
+  }
+]);
