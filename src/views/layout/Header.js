2d98a61ef78d14a3353784f69d3359d5a78a3066
Blake Ferm <blakeferm@gmail.com>
2021-03-08 19:27:38 -0500

    update weather icon to avoid cors issue

--- src/views/layout/Header.js
+++ src/views/layout/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import weatherIcon from "../../img/weather-title-icon.jpg";
 
 const Header = () => (
   <div className="header">
     <h1 className="header-title">
-      Weather Forecaster <i className="fas">&#xf6c4;</i>
+      Weather Forecaster{" "}
+      <img className="weather-title-icon" src={weatherIcon} alt="" />
     </h1>
   </div>
 );
