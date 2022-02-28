c9e6a80303f50842ec9b931fb26a62da2c99cc97
Blake Ferm <blakeferm@gmail.com>
2022-02-28 18:59:06 -0500

    update repo

--- src/views/DailyWeatherView.js
+++ src/views/DailyWeatherView.js
@@ -1,6 +1,6 @@
-import React from "react";
-import FormatTime from "../utils/FormatTime";
-import Accordion from "../components/Accordion";
+import React from 'react';
+import FormatTime from '../utils/FormatTime';
+import Accordion from '../components/Accordion';
 
 const DailyWeatherView = ({ dailyWeather, timezone, city }) => {
   const roundTemp = (tempToRound) => {
@@ -8,38 +8,38 @@ const DailyWeatherView = ({ dailyWeather, timezone, city }) => {
   };
 
   const formatTime = (timeToFormat) => {
-    return FormatTime(timeToFormat, timezone, "hA");
+    return FormatTime(timeToFormat, timezone, 'hA');
   };
 
   const formatDate = (dateToFormat) => {
-    return FormatTime(dateToFormat, timezone, "MM/DD");
+    return FormatTime(dateToFormat, timezone, 'MM/DD');
   };
 
   return (
-    <div className="daily-weather-data">
-      <p className="daily-weather-tagline">
-        8 Day Forecast |<span className="daily-weather-data-city"> {city}</span>
+    <div className='daily-weather-data'>
+      <p className='daily-weather-tagline'>
+        8 Day Forecast |<span className='daily-weather-data-city'> {city}</span>
       </p>
 
-      <Accordion title={"Click To View >>"} isExpand={false}>
-        <div className="daily-weather-data-box">
+      <Accordion title={'Click To View'} isExpand={false}>
+        <div className='daily-weather-data-box'>
           {dailyWeather &&
             dailyWeather.length > 0 &&
             dailyWeather.map((day, idx) => {
               return (
-                <span className="daily-weather-data-property" key={idx}>
-                  <p className="weather-data-title">
+                <span className='daily-weather-data-property' key={idx}>
+                  <p className='weather-data-title'>
                     Date | Time | Temp | High | Low | Conditions
                   </p>
-                  <p className="weather-data-value">
-                    {formatDate(day.dt)} | {formatTime(day.dt)} |{" "}
+                  <p className='weather-data-value'>
+                    {formatDate(day.dt)} | {formatTime(day.dt)} |{' '}
                     {roundTemp(day.temp.day)}&#176; | {roundTemp(day.temp.max)}
                     &#176; | {roundTemp(day.temp.min)}
                     &#176; | {day.weather[0].main}
                     <img
-                      className="daily-weather-data-icon"
+                      className='daily-weather-data-icon'
                       src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-                      alt=""
+                      alt=''
                     />
                   </p>
                 </span>
--- src/views/HourlyWeatherView.js
+++ src/views/HourlyWeatherView.js
@@ -1,6 +1,6 @@
-import React from "react";
-import FormatTime from "../utils/FormatTime";
-import Accordion from "../components/Accordion";
+import React from 'react';
+import FormatTime from '../utils/FormatTime';
+import Accordion from '../components/Accordion';
 
 const HourlyWeather = ({ hourlyWeather, timezone, city }) => {
   const roundTemp = (tempToRound) => {
@@ -8,36 +8,36 @@ const HourlyWeather = ({ hourlyWeather, timezone, city }) => {
   };
 
   const formatTime = (timeToFormat) => {
-    return FormatTime(timeToFormat, timezone, "hA");
+    return FormatTime(timeToFormat, timezone, 'hA');
   };
 
   const formatDate = (dateToFormat) => {
-    return FormatTime(dateToFormat, timezone, "MM/DD");
+    return FormatTime(dateToFormat, timezone, 'MM/DD');
   };
 
   return (
-    <div className="hourly-weather-data">
-      <p className="hourly-weather-tagline">
+    <div className='hourly-weather-data'>
+      <p className='hourly-weather-tagline'>
         48 Hour Forecast |
-        <span className="hourly-weather-data-city"> {city}</span>
+        <span className='hourly-weather-data-city'> {city}</span>
       </p>
 
-      <Accordion title={"Click To View >>"} isExpand={false}>
-        <div className="hourly-weather-data-box">
+      <Accordion title={'Click To View'} isExpand={false}>
+        <div className='hourly-weather-data-box'>
           {hourlyWeather.map((hour, idx) => {
             return (
-              <span className="hourly-weather-data-property" key={idx}>
-                <p className="weather-data-title">
+              <span className='hourly-weather-data-property' key={idx}>
+                <p className='weather-data-title'>
                   Date | Time | Temp | Feels Like | Conditions
                 </p>
-                <p className="weather-data-value">
-                  {formatDate(hour.dt)} | {formatTime(hour.dt)} |{" "}
+                <p className='weather-data-value'>
+                  {formatDate(hour.dt)} | {formatTime(hour.dt)} |{' '}
                   {roundTemp(hour.temp)}&#176; | {roundTemp(hour.feels_like)}
                   &#176; | {hour.weather[0].main}
                   <img
-                    className="daily-weather-data-icon"
+                    className='daily-weather-data-icon'
                     src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}@2x.png`}
-                    alt=""
+                    alt=''
                   />
                 </p>
               </span>
