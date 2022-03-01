64bd12097b395e144c171ed311c2cb1dcd6c2900
Blake Ferm <blakeferm@gmail.com>
2022-03-01 12:08:14 -0500

    update repo

--- src/views/layout/Header.js
+++ src/views/layout/Header.js
@@ -4,7 +4,7 @@ import weatherIcon from '../../img/weather-title-icon.jpg';
 const Header = () => (
   <div className='header'>
     <h1 className='header-title'>
-      Weather Forecaster!{' '}
+      Weather Forecaster{' '}
       <img className='weather-title-icon' src={weatherIcon} alt='' />
     </h1>
   </div>
